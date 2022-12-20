f343637cd1b27634e3cc282a0bbb1dcd3586a873
Jan Voges <voges@tnt.uni-hannover.de>
2022-12-20 11:32:44 +0100

    Add the entropy module

---
+++ entropy.py
@@ -0,0 +1,22 @@
+"""Code to compute the entropy of arbitrary data."""
+
+import collections
+import math
+
+
+def entropy(data, base=2):
+    """Compute the entropy of the data."""
+    if len(data) <= 1:
+        return 0.0
+
+    counts = collections.Counter()
+    for datum in data:
+        counts[datum] += 1
+
+    eta = 0.0
+    probs = [(float(c) / len(data)) for c in counts.values()]
+    for prob in probs:
+        if prob > 0.0:
+            eta -= prob * math.log(prob, base)
+
+    return eta
---
+++ test_entropy.py
@@ -0,0 +1,28 @@
+"""Tests for the entropy module."""
+
+import unittest
+import entropy
+
+
+class EntropyTestCase(unittest.TestCase):
+    """Test case for the entropy function."""
+
+    def test_no_data(self):
+        """Test correct behavior when no data is provided."""
+        self.assertEqual(entropy.entropy([]), 0.0)
+
+    def test_zero_entropy(self):
+        """Test correct behavior when the data has zero entropy."""
+        self.assertEqual(entropy.entropy([0, 0, 0]), 0.0)
+
+    def test_one_bit_entropy(self):
+        """Test for correct computation of one bit of entropy."""
+        self.assertEqual(entropy.entropy([0, 1]), 1.0)
+
+    def test_string_input(self):
+        """Test string input."""
+        self.assertAlmostEqual(entropy.entropy("GATTACA"), 1.84, places=2)
+
+
+if __name__ == "__main__":
+    unittest.main()
